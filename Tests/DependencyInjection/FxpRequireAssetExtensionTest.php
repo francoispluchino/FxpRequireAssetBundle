40969d3d68a7ff0c40cedd296810b209f9cfb9db
François Pluchino <francois.pluchino@gmail.com>
2014-11-05 15:53:07 +0100

    Use new version of require asset

--- Tests/DependencyInjection/FxpRequireAssetExtensionTest.php
+++ Tests/DependencyInjection/FxpRequireAssetExtensionTest.php
@@ -44,7 +44,7 @@ class FxpRequireAssetExtensionTest extends \PHPUnit_Framework_TestCase
     public function testCompileContainerWithExtension()
     {
         $container = $this->getContainer();
-        $this->assertTrue($container->hasDefinition('twig.extension.fxp_require_asset.inline_asset'));
+        $this->assertTrue($container->hasDefinition('twig.extension.fxp_require_asset'));
     }
 
     /**
